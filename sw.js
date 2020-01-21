e847cd89050190788cc2e535083b80a976406e05
faycel <faycel.3arab@gmail.com>
2020-01-21 22:09:30 +0100

    deleting useless files

--- sw.js
+++ sw.js
@@ -1,30 +1,27 @@
-importScripts('/cache-poly.js');
+importScripts("/cache-poly.js");
 
-const version = "0.6.18";
-const cacheName = `airhorner-${version}`;
+const version = "1.0.0";
+const cacheName = `smartlink-${version}`;
 
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open('smartlink').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/app.html',
-      ]);
-    })
+    caches.open("smartlink").then(function(cache) {
+      return cache.addAll(["/", "/index.html"]);
+    }),
   );
 });
 
-
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.open(cacheName)
+    caches
+      .open(cacheName)
       .then(cache => cache.match(event.request, { ignoreSearch: true }))
       .then(response => {
         return response || fetch(event.request);
-      })
+      }),
   );
 });
